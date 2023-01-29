4663638ffa7d32f75247ba0f0808d06f6d795a93
qikta <qikta.technolo7545@gmail.com>
2023-01-29 11:24:26 +0900

    add update and delete function.

--- lib/model/memo.dart
+++ lib/model/memo.dart
@@ -1,23 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Memo {
+  String id;
   String title;
   String detail;
   Timestamp createDate;
   Timestamp? updatedDate;
 
   Memo({
-   required this.title,
-   required this.detail,
+    required this.id,
+    required this.title,
+    required this.detail,
     required this.createDate,
     this.updatedDate,
-});
-}
-
-void test() {
-  Memo newMemo = Memo(
-      title: 'new memo',
-      detail: 'write more details',
-      createDate: Timestamp.now()
-  );
+  });
 }
--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -3,6 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_fire/model/memo.dart';
+import 'package:todo_fire/pages/add_edit_memo_page.dart';
 import 'package:todo_fire/pages/memo_detail_page.dart';
 
 class TopPage extends StatefulWidget {
@@ -15,27 +16,16 @@ class TopPage extends StatefulWidget {
 }
 
 class _TopPageState extends State<TopPage> {
-  List<Memo> memoList = [];
-  
-  Future<void> featchMemo() async {
-    final memoCollection = await FirebaseFirestore.instance.collection('memo').get();
-    final docs = memoCollection.docs;
-    for (var item in docs) {
-      Memo featchMemo = Memo(
-          title: item.data()['title'],
-          detail: item.data()['detail'],
-          createDate: item.data()['createDate']
-      );
-      memoList.add(featchMemo);
-    }
+  final memoCollection = FirebaseFirestore.instance.collection('memo');
 
-    setState(() {});
+  Future<void> deleteMemo(String id) async {
+    final docs = FirebaseFirestore.instance.collection('memo').doc(id);
+    await docs.delete();
   }
 
   @override
   void initState() {
     super.initState();
-    featchMemo();
   }
 
   @override
@@ -44,19 +34,71 @@ class _TopPageState extends State<TopPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-        itemCount: memoList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(memoList[index].title),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => MemoDetailPage(memoList[index])));
-            },
-          );
-      }),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: memoCollection.orderBy('createDate', descending: true).snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          if (!snapshot.hasData) {
+            return const Center(child: Text('memo not found...'));
+          }
+          final docs = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic> data = docs[index].data() as Map<String, dynamic>;
+              final Memo featchMemo = Memo(
+                  id: docs[index].id,
+                  title: data['title'],
+                  detail: data['detail'],
+                  createDate: data['createDate'],
+                  updatedDate: data['updateDate']);
+
+              return ListTile(
+                title: Text(featchMemo.title),
+                trailing: IconButton(
+                  onPressed: () {
+                    showModalBottomSheet(context: context, builder: (context) {
+                      return SafeArea(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ListTile(
+                              onTap: () {
+                                Navigator.pop(context);
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => AddEditMemoPage(currentMemo: featchMemo,)));
+                              },
+                              leading: const Icon(Icons.edit),
+                              title: const Text('edit'),
+                            ),
+                            ListTile(
+                              onTap: () async {
+                                await deleteMemo(featchMemo.id);
+                                Navigator.pop(context);
+                              },
+                              leading: const Icon(Icons.delete),
+                              title: Text('delete'),
+                            )
+                          ],
+                        ),
+                      );
+                    });
+                  },
+                  icon: const Icon(Icons.edit),
+                ),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => MemoDetailPage(featchMemo)));
+                },
+              );
+          });
+        }
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => AddEditMemoPage()));
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
